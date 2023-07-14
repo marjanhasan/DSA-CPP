efb3f4a6ab27b2306cc6444f13b92ab50c00440d
marjanhasan <moonmarjanhasan@gmail.com>
2023-07-14 23:48:35 +0600

    first problem done- sum of tree

--- module_20_hr_as_sum_of_tree.cpp
+++ module_20_hr_as_sum_of_tree.cpp
@@ -1,7 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    val == -1 ? root = NULL : root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *tleft, *tright;
+        l == -1 ? tleft = NULL : tleft = new Node(l);
+        r == -1 ? tright = NULL : tright = new Node(r);
+
+        p->left = tleft;
+        p->right = tright;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    queue<Node *> q;
+    q.push(root);
+    int sum = 0;
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+        sum += f->val;
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+    return sum;
+}
 int main()
 {
-
+    Node *root = input_tree();
+    int sum = level_order(root);
+    cout << sum << endl;
     return 0;
 }
